59a22f5f3c046940cad9a3d50856c11dfc8dcd30
Ankit Nayan <ankit@signoz.io>
2025-03-19 18:28:21 +0530

    read signozIngestionKey to env

--- vendors-signoz.cjs
+++ vendors-signoz.cjs
@@ -1,5 +1,5 @@
 const signozLogsUrl = "https://ingest.us.staging.signoz.cloud:443/v1/logs";
-const signozIngestionKey = "xxxx";
+const signozIngestionKey = process.env.SIGNOZ_INGESTION_KEY;
 const signozServiceName = "test-nodejs-service";
 
 module.exports = {
